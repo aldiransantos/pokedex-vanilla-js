8a37dfa8293cc2d9caf96f7c166dc3fd86a62ee2
Aldiran Santos <contact.aldiransantos@gmail.com>
2025-09-24 16:48:05 -0300

    feat: implementar fetch para consumir dados da API

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-console.log('hello');
+const listPokemon = document.getElementById('pokemon-list');
+console.log(listPokemon.text);
+
+async function fetchPokemon(id) {
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error('Carregando Pokedéx...');
+    }
+
+    const data = await response.json();
+    const pokemon = {
+      id: data.id,
+      name: data.name,
+      image: data.sprites.other['official-artwork'].front_default,
+      type: data.types.map((type) => type.type.name).join(', '),
+    };
+
+    return pokemon;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+async function getPokemon(number) {
+  for (let i = 1; i <= number; i++) {
+    pokemonData = await fetchPokemon(i);
+
+    listPokemon.innerHTML += `<li class="card">
+              <div class="card__header">
+                <span>${pokemonData.type}</span> <span>#${pokemonData.id}</span>
+              </div>
+              <img class="card__image" src="${pokemonData.image}" />
+              <p class="card__title">${pokemonData.name}</p>
+            </li>`;
+  }
+}
+
+getPokemon(8);
