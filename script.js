ef8cc8be587a3453de1e46582428e576b7ea052e
Aldiran Santos <contact.aldiransantos@gmail.com>
2025-09-24 20:17:23 -0300

    feat: Adiciona paginacao a lista de pokemons

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const listPokemon = document.getElementById('pokemon-list');
-console.log(listPokemon.text);
+const previousButton = document.getElementById('previous');
+const nextButton = document.getElementById('next');
+
+let limit = 3;
+let offset = 1;
 
 async function fetchPokemon(id) {
   const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
@@ -24,8 +28,10 @@ async function fetchPokemon(id) {
   }
 }
 
-async function getPokemon(number) {
-  for (let i = 1; i <= number; i++) {
+async function listPokemons(offset, limit) {
+  listPokemon.innerHTML = '';
+
+  for (let i = offset; i < offset + limit; i++) {
     pokemonData = await fetchPokemon(i);
 
     listPokemon.innerHTML += `<li class="card">
@@ -38,4 +44,56 @@ async function getPokemon(number) {
   }
 }
 
-getPokemon(8);
+function pagination() {
+  fetch(`https://pokeapi.co/api/v2/pokemon/`)
+    .then((res) => res.json())
+    .then((data) => {
+      const totalCount = data.count;
+      const totalPages = Math.ceil(totalCount / limit);
+      const paginationContainer = document.getElementById('pagination');
+      paginationContainer.innerHTML = '';
+
+      const currentPage = Math.ceil(offset / limit);
+
+      let startPage = 1;
+      let endPage = 3;
+
+      if (currentPage > 1) {
+        startPage = Math.max(1, currentPage - 1);
+        endPage = Math.min(totalPages, currentPage + 1);
+      }
+
+      for (let i = startPage; i <= endPage; i++) {
+        const button = document.createElement('button');
+        button.innerText = i;
+        if (i === currentPage) {
+          button.classList.add('active');
+        }
+
+        button.addEventListener('click', () => {
+          offset = (i - 1) * limit + 1;
+          listPokemons(offset, limit);
+
+          pagination();
+        });
+        paginationContainer.appendChild(button);
+      }
+    });
+}
+
+previousButton.addEventListener('click', () => {
+  if (offset > 1) {
+    offset -= limit;
+    listPokemons(offset, limit);
+    pagination();
+  }
+});
+
+nextButton.addEventListener('click', () => {
+  offset += limit;
+  listPokemons(offset, limit);
+  pagination();
+});
+
+listPokemons(offset, limit);
+pagination();
